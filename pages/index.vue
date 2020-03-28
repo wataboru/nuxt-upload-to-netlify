4c25953ace6d423dde4fe814ee41fd2187f1f23c
Wataru Sato <watarusatoh60@gmail.com>
2020-03-28 13:38:08 +0900

    Modify title in index

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        netlify-test
+        netlify-test by nuxt.js
       </h1>
       <h2 class="subtitle">
         test project for upload to netlify
